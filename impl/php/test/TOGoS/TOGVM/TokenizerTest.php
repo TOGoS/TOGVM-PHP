0fec08d6b5d61658385d29b7472e3283e504f918
TOGoS <togos00@gmail.com>
2014-12-05 13:12:13 -0600

    Rename some variables in TokenizerTest.

--- impl/php/test/TOGoS/TOGVM/TokenizerTest.php
+++ impl/php/test/TOGoS/TOGVM/TokenizerTest.php
@@ -16,12 +16,12 @@ class TOGoS_TOGVM_TokenizerTest extends PHPUnit_Framework_TestCase
 		$dh = opendir($tokenDir);
 		while( ($fn = readdir($dh)) !== false ) {
 			if( preg_match('/(.*)\.json$/', $fn, $bif) ) {
-				$jsonFile = $tokenDir."/".$fn;
-				$tokenFile = $tokenDir."/".$bif[1].".txt";
-				$json = file_get_contents($jsonFile);
-				$txt = file_get_contents($tokenFile);
-				$expected = EarthIT_JSON::decode($json);
-				$this->_testTokenize($expected, $txt, $tokenFile);
+				$tokenJsonFile = $tokenDir."/".$fn;
+				$sourceFile = $tokenDir."/".$bif[1].".txt";
+				$tokenJson = file_get_contents($tokenJsonFile);
+				$source = file_get_contents($sourceFile);
+				$expectedTokens = EarthIT_JSON::decode($tokenJson);
+				$this->_testTokenize($expectedTokens, $source, $sourceFile);
 			}
 		}
 		closedir($dh);
