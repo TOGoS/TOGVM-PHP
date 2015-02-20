95c39394372a5b3af3077ffabf335d7dcd7b91b5
TOGoS <togos00@gmail.com>
2015-02-20 12:50:46 -0600

    Fix identification of sha1: URNs.

--- lib/TOGoS/TOGES/HashURNSymbolTable.php
+++ lib/TOGoS/TOGES/HashURNSymbolTable.php
@@ -3,10 +3,10 @@
 class TOGoS_TOGES_HashURNSymbolTable extends TOGoS_TOGVM_FakeArray
 {
 	public function offsetExists($k) {
-		return preg_match('/^urn:(?:sha1|bitprint):([A-Z2-7]{32})\W/',$k);
+		return preg_match('/^urn:(?:sha1|bitprint):([A-Z2-7]{32})/',$k);
 	}
 	public function offsetGet($k) {
-		if( preg_match('/^urn:(?:sha1|bitprint):([A-Z2-7]{32})\W/',$k) ) {
+		if( preg_match('/^urn:(?:sha1|bitprint):([A-Z2-7]{32})/',$k) ) {
 			return [
 				'classUri' => 'http://ns.nuke24.net/TOGVM/Expressions/FunctionApplication',
 				'functionUri' => 'http://ns.nuke24.net/TOGVM/Functions/ResolveHashURN',
