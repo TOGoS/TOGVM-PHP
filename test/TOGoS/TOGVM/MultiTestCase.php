7e1ebd7c5c69ee94d2f4c87c481340c046dec81b
TOGoS <togos00@gmail.com>
2015-02-27 18:48:53 -0600

    Move some helper functions out of MultiTestCase into new parent class, TestCase.

--- test/TOGoS/TOGVM/MultiTestCase.php
+++ test/TOGoS/TOGVM/MultiTestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class TOGoS_TOGVM_MultiTestCase extends PHPUnit_Framework_TestCase
+abstract class TOGoS_TOGVM_MultiTestCase extends TOGoS_TOGVM_TestCase
 {
 	protected abstract function _testFilePair( $input, $inputFilename, $expectedOutput, $expectedOutputFilename );
 	
@@ -8,28 +8,6 @@ abstract class TOGoS_TOGVM_MultiTestCase extends PHPUnit_Framework_TestCase
 	
 	/** @return array(input extension, expectation extension) */
 	protected abstract function getTestVectorExtensions();
-	
-	protected function findTestVectorDirectory() {
-		for( $dir = __DIR__, $x=0; is_dir($dir) and $x<20; $dir = dirname($dir), ++$x ) {
-			if( is_dir($tvDir = "$dir/test-vectors") ) return $tvDir;
-			if( is_dir($tvDir = "$dir/vendor/togos/togvm-spec/test-vectors") ) return $tvDir;
-		}
-		throw new Exception("Couldn't find 'test-vectors' directory.");
-	}
-	
-	protected $testVectorDir;
-	protected function getTestVectorDirectory() {
-		if( $this->testVectorDir === null ) {
-			$this->testVectorDir = $this->findTestVectorDirectory();
-		}
-		return $this->testVectorDir;
-	}
-
-	protected function getTestLanguageConfig() {
-		$config = TOGoS_TOGES_Util::loadOperators($this->getTestVectorDirectory().'/test-language.json');
-		$config['flushingOperators'] = ["\n"];
-		return $config;
-	}
 	
 	protected function getTestVectorFilePairs() {
 		$testVectorSubdir = $this->getTestVectorDirectory().'/'.$this->getTestVectorSubdirectoryName();
---
+++ test/TOGoS/TOGVM/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+abstract class TOGoS_TOGVM_TestCase extends PHPUnit_Framework_TestCase
+{
+	protected function findTestVectorDirectory() {
+		for( $dir = __DIR__, $x=0; is_dir($dir) and $x<20; $dir = dirname($dir), ++$x ) {
+			if( is_dir($tvDir = "$dir/test-vectors") ) return $tvDir;
+			if( is_dir($tvDir = "$dir/vendor/togos/togvm-spec/test-vectors") ) return $tvDir;
+		}
+		throw new Exception("Couldn't find 'test-vectors' directory.");
+	}
+	
+	protected $testVectorDir;
+	protected function getTestVectorDirectory() {
+		if( $this->testVectorDir === null ) {
+			$this->testVectorDir = $this->findTestVectorDirectory();
+		}
+		return $this->testVectorDir;
+	}
+
+	protected function getTestLanguageConfig() {
+		$config = TOGoS_TOGES_Util::loadOperators($this->getTestVectorDirectory().'/test-language.json');
+		$config['flushingOperators'] = ["\n"];
+		return $config;
+	}
+}
