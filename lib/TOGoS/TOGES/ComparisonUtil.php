f4f205eb19335b36e0547bfff542247a2e17e332
TOGoS <togos00@gmail.com>
2016-03-25 13:43:02 -0500

    Add functions for fixing expressions like a < b < c

---
+++ lib/TOGoS/TOGES/ComparisonUtil.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @api
+ */
+class TOGoS_TOGES_ComparisonUtil
+{
+	public static function isComparisonOp( array $expr ) {
+		if( $expr['classRef'] != 'http://ns.nuke24.net/TOGVM/Expressions/FunctionApplication' ) return false;
+		
+		switch( $expr['functionRef'] ) {
+		case 'http://ns.nuke24.net/TOGVM/Functions/IsGreaterThanOrEqualTo':
+		case 'http://ns.nuke24.net/TOGVM/Functions/IsGreaterThan':
+		case 'http://ns.nuke24.net/TOGVM/Functions/AreEqual':
+		case 'http://ns.nuke24.net/TOGVM/Functions/AreNotEqual':
+		case 'http://ns.nuke24.net/TOGVM/Functions/IsLesserThan':
+		case 'http://ns.nuke24.net/TOGVM/Functions/IsLesserThanOrEqualTo':
+			return true;
+		}
+	}
+	
+	protected static function collectComparisonOps( array $expr, array &$toBeAnded, &$firstValueExpr=null, &$lastValueExpr=null ) {
+		if( !self::isComparisonOp($expr) ) throw new Exception("Not a comparison op!");
+		
+		$firstValueExpr = $expr['arguments'][0];
+		$lastValueExpr = $expr['arguments'][1];
+
+		if( !self::isComparisonOp($firstValueExpr) and !self::isComparisonOp($lastValueExpr) ) {
+			// No transform to be done!
+			$toBeAnded[] = $expr;
+			return;
+		}
+		
+		$leftValueExpr = $firstValueExpr;
+		$rightValueExpr = $lastValueExpr;
+		
+		if( self::isComparisonOp($firstValueExpr) ) {
+			self::collectComparisonOps($firstValueExpr, $toBeAnded, $firstValueExpr, $leftValueExpr );
+		}
+		$toBeAndedRight = array();
+		if( self::isComparisonOp($lastValueExpr) ) {
+			self::collectComparisonOps($lastValueExpr, $toBeAndedRight, $rightValueExpr, $lastValueExpr );
+		}
+		
+		// Rewrite the middle expression
+		$expr['arguments'][0] = $leftValueExpr;
+		$expr['arguments'][1] = $rightValueExpr;
+		$toBeAnded[] = $expr;
+		$toBeAnded = array_merge($toBeAnded, $toBeAndedRight);
+	}
+	
+	/**
+	 * In some languages you may want to allow expressions like
+	 * 
+	 *   a < b = c <= d
+	 * 
+	 * and have them be equivalent to
+	 * 
+	 *   a < b and b = c and c <= d
+	 *
+	 * This function will do that transformation for you.
+	 * 
+	 * It must be called on the OUTERMOST comparison expression or it won't work!
+	 */
+	public static function fixMultiComparisonOp( array $expr ) {
+		if( !self::isComparisonOp($expr) ) return $expr;
+		
+		$toBeAnded = array();
+		self::collectComparisonOps($expr, $toBeAnded);
+		if( count($toBeAnded) == 1 ) return $toBeAnded[0];
+		
+		return array(
+			'classRef' => 'http://ns.nuke24.net/TOGVM/Expressions/FunctionApplication',
+			'functionRef' => 'http://ns.nuke24.net/TOGVM/Functions/And',
+			'arguments' => $toBeAnded,
+			'sourceLocation' => isset($expr['sourceLocation']) ? $expr['sourceLocation'] : null,
+		);
+	}
+	
+	/**
+	 * Call this on your top-level expression
+	 */
+	public static function fixProgramWithMultiComparisonOps( array $expr, TOGoS_TOGES_ExpressionTransformer $transformer ) {
+		return $transformer->transformRecursively( $expr, array(__CLASS__, 'fixMultiComparisonOp'), null );
+	}
+}
---
+++ lib/TOGoS/TOGES/ExpressionTransformer.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @api
+ */
+class TOGoS_TOGES_ExpressionTransformer
+{
+	public static function identity($thing) {
+		return $thing;
+	}
+
+	public static function identityFunction( ) {
+		return array(__CLASS__,'identity');
+	}
+	
+	protected function transformSubExpressionsRecursively( $expr, $preTransform, $postTransform ) {
+		switch( $expr['classRef'] ) {
+		case 'http://ns.nuke24.net/TOGVM/Expressions/LiteralString':
+		case 'http://ns.nuke24.net/TOGVM/Expressions/LiteralInteger':
+		case 'http://ns.nuke24.net/TOGVM/Expressions/LiteralNumber':
+		case 'http://ns.nuke24.net/TOGVM/Expressions/LiteralBoolean':
+		case 'http://ns.nuke24.net/TOGVM/Expressions/Variable':
+			// No recursion to be done!
+			return $expr;
+		case 'http://ns.nuke24.net/TOGVM/Expressions/FunctionApplication':
+			if( isset($expr['function']) ) {
+				$expr['function'] = self::transformRecursively( $expr['function'], $preTransform, $postTransform );
+			}
+			foreach( $expr['arguments'] as $k=>$argument ) {
+				$expr['arguments'][$k] = self::transformRecursively( $argument, $preTransform, $postTransform );
+			}
+			return $expr;
+		default:
+			// TODO: It's entirely 'arguments' that appear to be sub-expressions, we can handle that!
+			throw new Exception("Unrecognized expression class URI: '{$expr['classRef']}'");
+		}
+	}
+	
+	/**
+	 * Apply a transformation to an expression and all sub-expressions
+	 * 
+	 * @param callable $preTransform a transformation to be applied ~before~ recursion
+	 *   (outermost expression will be transformed first)
+	 * @param callable $postTransform a transformation to be applied ~after~ recursion
+	 *   (innermost expressions will be transformed first)
+	 */
+	public function transformRecursively( array $expr, $preTransform=null, $postTransform=null ) {
+		if(  $preTransform === null )  $preTransform = self::identityFunction();
+		if( $postTransform === null ) $postTransform = self::identityFunction();
+
+		$expr = call_user_func($preTransform, $expr);
+		$expr = $this->transformSubExpressionsRecursively( $expr, $preTransform, $postTransform );
+		return call_user_func($postTransform, $expr);
+	}
+}
